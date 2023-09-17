f0f93481739d4fa35237250a9636c53758ebb494
qwrznn <annazukovska6@gmail.com>
2023-09-17 13:32:07 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>first</title>
+	</head>
+
+	<body>
+		<h1><?php 
+
+        $str1 = "Hello,";
+        $str2 = " PHP!";
+        echo $str1 . $str2; 
+
+        ?></h1>
+        <hr>
+        <a href="sum.php">Sum</a>
+	</body>
+
+
+</html>
---
+++ sum.php
@@ -0,0 +1,13 @@
+<?php
+
+// $x = 3;
+// $y = 6.5;
+// echo $x * round($y);
+// echo "<br>";
+// echo $x - (int)$y;
+
+$a = 1;
+$b=2;
+$c=3;
+$d=4;
+echo ($a * $b) + ($c * $d);
