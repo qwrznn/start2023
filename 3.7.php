e55254f4fae3fbe6df21b986e05f1f53f9fb1202
qwrznn <145158478+qwrznn@users.noreply.github.com>
2023-10-10 12:00:13 +0300

    Update 3.7.php

--- 3.7.php
+++ 3.7.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,32 +7,16 @@
 </head>
 <body>
     <h1>3.7 Термометр</h1>
-    <table>
-        <tr>
-
-            // $n = -20;
-            // $i = 2;
-            // for ($i = 20; $i <= $n; $i--)
-            //    echo $i;
-            
-        </tr>
-    </table>
-</body>
-</html> -->
-
 <table>
             <?php
             $n = rand(20, -20);
-            for ($i = 20; $i >= $n; $i--)
+            for ($i = 20; $i >= $n; $i--){
             echo "<tr><td>$i</td><td style=width:30px;background:yellow></td></tr>\n";
-
-            for ($i = $n; ; $i--){
-                if ($i > -20) {
+            }
+            for ($i = $n-1; $i >= -20; $i--){
                 echo "<tr><td>$i</td><td style=width:30px;background:red></td></tr>\n";
-                }
-                else {
-                    break;
-                }
             }
             ?>
 </table>
+</body>
+</html>
